8b5f5bf24f8c81132f3092b80a4f77856121a3f6
jungdw0624 <jdw8998@daum.net>
2020-07-16 15:16:46 +0900

    Initial Commit

---
+++ Rooms/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rooms
+//
+//  Created by kp_justin on 2020/07/16.
+//  Copyright © 2020 justin dongwook Jung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
